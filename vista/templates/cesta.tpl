41c1e9b7d7bbefdb5578bf84c3c07043e28c258e
david <david@DESKTOP-ADK2CDJ>
2019-06-06 02:22:01 +0200

    4.5 version paypal

--- vista/templates/cesta.tpl
+++ vista/templates/cesta.tpl
@@ -12,6 +12,30 @@
 
     {/foreach}
     <strong>Importe total:<strong>{$total}€
+            <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+                <!--Datos para pagar paypal-->
+                <input name="cmd" type="hidden" value="_cart" />
+                <input name="upload" type="hidden" value="1" />
+                <!--Mi correo de pay pall-->
+                <input name="business" type="hidden" value="Zaraguaza87-facilitator@iesandorra.es" />
+                <input name="shopping_url" type="hidden" value="http://vista/templates/pago.tpl" />
+                <input name="currency_code" type="hidden" value="EUR" />
+                <input name="return" type="hidden" value="http://vista/templates/pago.tpl%22/%3E
+                <input name="notify_url" type="hidden" value="http://vista/templates/pago.tpl" />
+                <input name="rm" type="hidden" value="2" />
+
+                <!--pasamos el contenido de la factura y mostramos el boton ( foto de paypal) -->
+                <tr class="pago">
+                  <td class="pago"></td>
+                  <td class="pago"></td>
+                  <td class="pago"></td>
+                    <input type="hidden" name="item_name_total" value="total">
+                    <input type="hidden" name="amount_1" value="1">
+                    <input type="hidden" name="quantity_total" value="{$total}">
+                </tr>
+                <input type="image" src="http://www.paypal.com/es_ES/i/btn/x-click-but01.gif" border="0" name="submit" alt="Realice pagos con PayPal: es rápido, gratis y seguro">
+        </form>
+
     <form action='productos.php' method='POST'>
     <input type="submit" value='vaciar' name="Vaciar"/>
     <input type="submit" value='pagar' name="Pagar"/>
--- vista/templates_c/dacdf2d6088d09ea50eea50054130f0f3bb665c9_0.file.cesta.tpl.php
+++ vista/templates_c/dacdf2d6088d09ea50eea50054130f0f3bb665c9_0.file.cesta.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2019-06-05 21:19:35
+/* Smarty version 3.1.33, created on 2019-06-06 00:10:07
   from 'D:\wamp64\www\tienda\vista\templates\cesta.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5cf831e78af6f7_14647827',
+  'unifunc' => 'content_5cf859dfe78332_46384975',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'dacdf2d6088d09ea50eea50054130f0f3bb665c9' => 
     array (
       0 => 'D:\\wamp64\\www\\tienda\\vista\\templates\\cesta.tpl',
-      1 => 1559769021,
+      1 => 1559779799,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5cf831e78af6f7_14647827 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5cf859dfe78332_46384975 (Smarty_Internal_Template $_smarty_tpl) {
 if ((isset($_smarty_tpl->tpl_vars['productos']->value))) {?>
 <h2>Listado de cesta</h2>
     <?php $_smarty_tpl->_assignInScope('total', 0);?>
@@ -47,6 +47,31 @@ foreach ($_from as $_smarty_tpl->tpl_vars['producto']->value => $_smarty_tpl->tp
 $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     <strong>Importe total:<strong><?php echo $_smarty_tpl->tpl_vars['total']->value;?>
 €
+            <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+                <!--Datos para pagar paypal-->
+                <input name="cmd" type="hidden" value="_cart" />
+                <input name="upload" type="hidden" value="1" />
+                <!--Mi correo de pay pall-->
+                <input name="business" type="hidden" value="Zaraguaza87-facilitator@iesandorra.es" />
+                <input name="shopping_url" type="hidden" value="http://vista/templates/pago.tpl" />
+                <input name="currency_code" type="hidden" value="EUR" />
+                <input name="return" type="hidden" value="http://vista/templates/pago.tpl%22/%3E
+                <input name="notify_url" type="hidden" value="http://vista/templates/pago.tpl" />
+                <input name="rm" type="hidden" value="2" />
+
+                <!--pasamos el contenido de la factura y mostramos el boton ( foto de paypal) -->
+                <tr class="pago">
+                  <td class="pago"></td>
+                  <td class="pago"></td>
+                  <td class="pago"></td>
+   <input type="hidden" name="item_name" value="Compra de <?php echo $_smarty_tpl->tpl_vars['usuario']->value;?>
+">
+   <input type="hidden" name="amount" value="<?php echo $_smarty_tpl->tpl_vars['total']->value;?>
+">
+                </tr>
+                <input type="image" src="http://www.paypal.com/es_ES/i/btn/x-click-but01.gif" border="0" name="submit" alt="Realice pagos con PayPal: es rápido, gratis y seguro">
+        </form>
+
     <form action='productos.php' method='POST'>
     <input type="submit" value='vaciar' name="Vaciar"/>
     <input type="submit" value='pagar' name="Pagar"/>
