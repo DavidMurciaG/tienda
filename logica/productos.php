1a9477ed6a8e356d01d0f7e95e7d0c7cc4906701
DavidMurciaG <davidmurcia_18@hotmail.com>
2019-06-11 23:48:44 +0200

    cambios con el paypal

--- logica/productos.php
+++ logica/productos.php
@@ -1,58 +1,68 @@
 <?php
 
 session_start();
-include '..\clases\DB.php';
-include '..\clases\Cesta.php';
-include_once '..\clases\Producto.php';
-
 require('Smarty.class.php');
+        const D = DIRECTORY_SEPARATOR;
+include_once '..' . D . 'clases' . D . 'DB.php';
+include_once '..' . D . 'clases' . D . 'Cesta.php';
+include_once '..' . D . 'clases' . D . 'Producto.php';
+
 $smarty = new Smarty;
 $smarty->template_dir = '..\vista\templates';
 $smarty->compile_dir = '..\vista\templates_c';
 
-if (isset($_SESSION['usuario'])) {
-    $smarty->assign("usuario", $_SESSION['usuario']);
-    $lista = DB::obtieneProductos();
-    foreach ($lista as $list) {
-        $pvp = $list->getPVP();
-        $cod = $list->getcodigo();
-        $nc = $list->getnombrecorto();
-        $listado .="<form action='productos.php' method='POST'>.<input type='submit' value='Añadir' name='añadir'>"
-                . " $nc: $pvp €<br>"
-                . "<input type='hidden' value='$cod' name='cod'>"
-                . "</form>";
-        $smarty->assign("listado", $listado);
-    }
-} else {
-    header('Location: http://localhost/tienda/logica/login.php');
+$productos = null;
+
+if (!isset($_SESSION['usuario'])) {
+    header('Location:login.php?error=Debes identificarse');
+    exit();
 }
+//pasamos el valor del nombre de usuario conectado a la vista
+$smarty->assign("usuario", $_SESSION['usuario']);
+
+//llamamos al metodo ObtieneProductos ( nos devuelve un array de Producto ) 
+$lista_productos = DB::obtieneProductos();
+
+//recorremos la lista y vamos guardando el contenido en variables para dar formato a la lista:
+foreach ($lista_productos as $objetoProducto) {
+    $precio = $objetoProducto->getPVP();
+    $codigo = $objetoProducto->getcodigo();
+    $nombre_corto = $objetoProducto->getnombrecorto();
+//damos formato y guardamos el codigo en un hidden. (un form para cada producto y boton.
+    $listado .= "<form action='productos.php' method='POST'>"
+            . "<input type='submit' value='añadir' name='submit'>"
+            . " $nombre_corto | <strong> $precio € </strong><br>"
+            . "<input type='hidden' value='$codigo' name='cod'>"
+            . "</form>";
+//pasamos los valores con formato a Profuctos.tpl con la variable $listado.
+    $smarty->assign("listado", $listado);
+}
+
+//iniciamos la cesta
 $cesta = Cesta::obtener_cesta();
 
-if (isset($_POST['añadir'])) {
+//(al dar a AÑADIR) lo guardamos con el cod como indice mediante add_producto
+if (isset($_POST['submit'])) {
     $cod = $_POST['cod'];
     $cesta->add_producto($cod);
-    $productos = $cesta->getProductos();
-    $cesta->guardar_cesta();
 }
-
-if (isset($_POST['descontar'])) {
+//al dar a Quitar, quitamos ese producto del 
+if (isset($_POST['quitar'])) {
     $cod = $_POST['cod'];
     $cesta->restaProducto($cod);
-    $productos = $cesta->getProductos();
-    $cesta->guardar_cesta();
+}
+//al dar a Vaciar:
+if (isset($_POST['Vaciar'])) {
+    $cesta->vaciar_cesta();
+    //destruimos la variable cesta
 }
 
-if (isset($_POST['vaciar'])) {
-    $cesta->vaciar_cesta();
-    $cesta->guardar_cesta();
-    unset($cesta);
-}
-if (isset($_POST['pagar'])) {
-    $productos = $cesta->getProductos();
-    header('Location: http://localhost/tienda/logica/pagar.php');
-    $smarty->display('pagar.tpl');
-}
+$productos = $cesta->getProductos();
+
+$cesta->guardar_cesta();
+//pasamos la lista de productos
+//
+
 $smarty->assign("productos", $productos);
 $smarty->display('productos.tpl');
 ?>
-
